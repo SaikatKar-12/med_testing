7d2e77ec1aa423c743aed47cd65f439009ce5c15
Saikat Kar <ksaikat009@gmail.com>
2025-04-25 01:03:05 +0530

    login

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 
 const MedicineController = require('../../controllers/medicine-controller');
+const HospitalController = require('../../controllers/hospital-controller');
 
 
 const router = express.Router();
@@ -10,6 +11,20 @@ router.delete('/medicine/:id',MedicineController.destroy);
 router.get('/medicine/:id',MedicineController.get);
 router.get('/medicine', MedicineController.getAll);
 router.patch('/medicine/:id',MedicineController.update);
+
+router.post(
+    '/signup', 
+    HospitalController.create
+);
+router.post(
+    '/signin',
+    HospitalController.signIn
+);
+
+router.get(
+    '/isAuthenticated',
+    HospitalController.isAuthenticated
+);
 
 
 module.exports = router;
